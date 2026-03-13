ec8a3ca395e3931b0898855a5318f00e252714af
sophonsim <sophon.saesim@gmail.com>
2026-03-13 09:04:55 +0000

    revise readme

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # readcsvdata
-This repo is to practice reading CSV data for data engineer project. 
-from source of ERP and CRM system.
+This repo practice data engineer project by reading CSV data for external source(github folder) . 
 
-** The readme will be modified later. 
+Project Concept
+ingest data from external system(assume github). process it with the medallion concept. 
+
+Project summary
+0. created project environment
+1. extract raw data
+2. load clean up data
+3. transform business ready data 
+4. create data pipeline
+5. create dashboard
+
+Project Details
+0. created project environment
+    created environment of project by setup file
+
+1. extract raw data
+    extract raw data from volume to bronze layer
+
+2. load clean up data   
+    load clean up data from bronze to silver
+
+3. transform business ready data 
+    transform clean up data in to business ready data
+
+4. created data pipeline    
+    issue data pipeline
+
+5. create business dashboard    
+    issue dashboard
+
+
+** The readme will be modified later**
+
--- scripts/0_initial_project/setup_config.py
+++ scripts/0_initial_project/setup_config.py
@@ -4,35 +4,34 @@ schema_setup = ["bronze", "silver", "gold"]
 volume_setup = ["source_crm", "source_erp"]
 
 # github base url
-github_url = "https://raw.githubusercontent.com/sophonsim/readcsvdata/refs/heads/main/dataset/source/"
+github_url = "https://raw.githubusercontent.com/sophonsim/readcsvdata/refs/heads/main/dataset/source"
+target = f"/Volumes/{catalog_setup}/bronze"
 
 # source data copy
 SOURCE_CONFIG = [
     {
-        "url" : f"{github_url}/source_crm/cust_info.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_crm/cust_info.csv"
+        "url" : f"{github_url}/source_crm/cust_info.csv",
+        "target" : f"{target}/source_crm/cust_info.csv"
     },
     {
-        "url" : f"{github_url}/source_crm/prd_info.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_crm/prd_info.csv"
+        "url" : f"{github_url}/source_crm/prd_info.csv",
+        "target" : f"{target}/source_crm/prd_info.csv"
     },
     {
-        "url" : f"{github_url}/source_crm/sales_details.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_crm/sales_details.csv"
+        "url" : f"{github_url}/source_crm/sales_details.csv",
+        "target" : f"{target}/source_crm/sales_details.csv"
     },
     {
-        "url" : f"{github_url}/source_erp/CUST_AZ12.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_erp/CUST_AZ12.csv"
+        "url" : f"{github_url}/source_erp/CUST_AZ12.csv",
+        "target" : f"{target}/source_erp/CUST_AZ12.csv"
     },
     {
-        "url" : f"{github_url}/source_erp/LOC_A101.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_er/LOC_A101.csv"
+        "url" : f"{github_url}/source_erp/LOC_A101.csv",
+        "target" : f"{target}/source_erp/LOC_A101.csv"
     },
      {
-        "url" : f"{github_url}/source_erp/PX_CAT_G1V2.csv$0",
-        "target" : f"/Volumes/{catalog_setup}/bronze/source_erp/PX_CAT_G1V2.csv"
+        "url" : f"{github_url}/source_erp/PX_CAT_G1V2.csv",
+        "target" : f"{target}/source_erp/PX_CAT_G1V2.csv"
     }
     
 ]
-
-
